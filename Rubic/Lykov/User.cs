7c97e997b0eaf33f479246c0736b037fde089885
Innaviton <83660115+Likeout@users.noreply.github.com>
2022-03-10 18:50:35 +0300

    Добавил user

---
+++ Rubic/Lykov/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rubic.Lykov
+{
+    public class User
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public int age { get; set; }
+
+        public bool isBoy { get;set }
+
+    }
+}
