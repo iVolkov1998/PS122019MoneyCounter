c41389924660f4b01f5089ac93d4d312337d8124
Yana22800 <yanusik1043@gmail.com>
2022-03-10 19:18:55 +0300

    comid

--- Rubic/Yana/User.cs
+++ Rubic/Yana/User.cs
@@ -7,5 +7,17 @@ namespace Rubic.Yana
 {
     public class User
     {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public bool Gender { get; set; }
+        public int Age { get; set; }
+        public string Rassa { get; set; }
+        public string Species { get; set; }
+        public string Mind { get; set; }
+
+
+
+
+
     }
 }
