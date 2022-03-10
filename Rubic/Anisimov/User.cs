d9b12baa8014b45d53ca698e4632b93654a5705e
Diwax1 <andylolka03@gmail.com>
2022-03-10 18:53:40 +0300

    Создал таблицу пользователя

---
+++ Rubic/Anisimov/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rubic.Anisimov
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Patronymic { get; set; }
+        public int PhoneNumberPrefix { get; set; }
+        public int PhoneNumber { get; set; }
+        public bool IsBoy { get; set; }
+        public DateTime BirthdayDate { get; set; }
+        public DateTime RegestrationDate { get; set; }
+        public int Adress { get; set; }
+        public int YearsOld { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+    }
+}
