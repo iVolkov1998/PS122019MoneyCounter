328ca5ca9289ed8b086961f23803f80293c5473a
512143 <dan.kedrov@yandex.ru>
2022-03-10 18:53:32 +0300

    Создал профиль пользователя(таблица)

---
+++ Rubic/Kedrov/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rubic.Kedrov
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string patronymic { get; set; }
+        public DateTime RegTime { get; set; }
+        public DateTime BirthTime { get; set; }
+        public bool IsBoy { get; set; }
+        public string Mail  { get; set; }
+        public string Status { get; set; }
+        public string Publications { get; set; }
+
+    }
+}
