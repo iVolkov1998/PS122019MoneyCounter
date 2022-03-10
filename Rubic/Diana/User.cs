3a2e4c753208ceaa5cc3758c5853ec349777da4b
Diana001a <di89101630730@mail.ru>
2022-03-10 19:11:44 +0300

    создала таблицу юзера

--- Rubic/Diana/User.cs
+++ Rubic/Diana/User.cs
@@ -7,5 +7,12 @@ namespace Rubic.Diana
 {
     public class User
     {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public bool Gender { get; set; }
+        public int Age { get; set; }
+        public string Rassa { get; set; }
+        public string Species { get; set; }
+        public string Mind { get; set; }
     }
 }
