467968ef92cc0b8cf7525080f2db1732c5f366ec
rusumuhu <83660075+rusumuhu@users.noreply.github.com>
2022-03-10 18:54:59 +0300

    Создал таблицу пользователя

---
+++ Rubic/Tsarev/TableUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rubic.Tsarev
+{
+    public class TableUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public bool Gender { get; set; }
+        public string Old { get; set; }
+        public string Nationality { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Password { get; set; }
+
+    }
+}
