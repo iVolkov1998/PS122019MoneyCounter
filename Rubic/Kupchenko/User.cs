6eb6b4cde3a5bde7d46ea02719be5ff9c718db96
Tatyana-Essentya <listov0@bk.ru>
2022-03-10 18:54:09 +0300

    создал табличку пользователя

---
+++ Rubic/Kupchenko/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rubic.Kupchenko
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int PhoneNumber { get; set; }
+        public string County { get; set; }
+        public string Login { get; set; }
+        public int Password { get; set; }
+        public bool IsBoy { get; set; }
+        public int YearsOld { get; set; } 
+        public int BirthdayDate { get; set; }
+        public int RegisterDate { get; set; }
+        public int Adress { get; set; }
+        public int Street { get; set; }
+
+    }
+}
