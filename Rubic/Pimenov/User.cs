94bf8c4902ec94a4032e1822069b6e413aca115f
ivashka06 <>
2022-03-10 19:15:45 +0300

    Сделали юзер

--- Rubic/Pimenov/User.cs
+++ Rubic/Pimenov/User.cs
@@ -7,5 +7,19 @@ namespace Rubic.Pimenov
 {
     public class User
     {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public int PhoneNumberPrefix { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public int Age { get; set; }
+        public bool IsBoy { get; set; }
+        public int Growth { get; set; }
+        public DateTime DateOfBirth { get; set; } 
+
+
     }
 }
