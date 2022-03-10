58c4044722b0e1d99836554c4ed74f6ed7921485
rusumuhu <nicitagerts@yandex.ru>
2022-03-10 19:11:15 +0300

    Создали табличку пользователя

--- Rubic/Glyackin/TablerUser.cs
+++ Rubic/Glyackin/TablerUser.cs
@@ -7,5 +7,14 @@ namespace Rubic.Glyackin
 {
     public class TablerUser
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Password { get; set; }
+        public bool Sex { get; set; }
+        public string  Old { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Nation { get; set; }
+        public int Patronymic { get; set; }
     }
 }
