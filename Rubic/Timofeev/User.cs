0e0b8bdb4fedcea3c57fdad54ea41e82d6de48c1
Am1go1 <ivantimofeev930@gmail.com>
2022-03-10 18:54:12 +0300

    создал обычного пользователя

---
+++ Rubic/Timofeev/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rubic.Timofeev
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Species { get; set; }
+        public int PhoneNumberPrefix { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public string Password { get; set; }
+        
+    }
+}
