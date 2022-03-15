4f7aa1dea6a8b8215d59c4aee2fe6a27af312f91
Diwax1 <andylolka03@gmail.com>
2022-03-15 19:26:31 +0300

    Создал контекст

---
+++ Rubic/DbContext/Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rubic.DbContext
+{
+    public class Context : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public Context(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ Rubic/DbContext/IContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rubic.DbContext
+{
+    public interface IContext : IDisposable, IAsyncDisposable
+    {
+        DbSet<Profile> Profiles { get; set; }
+        DbSet<SavedVideo> SavedVideos { get; set; }
+    }
+}
